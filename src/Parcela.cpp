19c646385efbccac8ca7bb11e2b2f070466679d5
Brian Stanley <brianelblog@gmail.com>
2018-10-17 23:37:14 -0300

    fix proyect

---
+++ src/Parcela.cpp
@@ -0,0 +1,18 @@
+/*
+ * Parcela.cpp
+ *
+ *  Created on: Oct 17, 2018
+ *      Author: brian
+ */
+
+#include "Parcela.h"
+
+Parcela::Parcela() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Parcela::~Parcela() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Parcela.h
@@ -0,0 +1,17 @@
+/*
+ * Parcela.h
+ *
+ *  Created on: Oct 17, 2018
+ *      Author: brian
+ */
+
+#ifndef SRC_PARCELA_H_
+#define SRC_PARCELA_H_
+
+class Parcela {
+public:
+	Parcela();
+	virtual ~Parcela();
+};
+
+#endif /* SRC_PARCELA_H_ */
