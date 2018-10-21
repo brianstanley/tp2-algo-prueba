48f8dfc9f68e2c24413f2798ab12b01b61909060
german <germandawi@gmail.com>
2018-10-21 12:24:14 -0300

    cree la carpeta libreria donde van los archivos de EasyBMP. tambiencomence la creacion de la clase imagen_de_tablero que crea los archivos.bmp para mostrar el tablero

---
+++ src/imagen_de_tablero.cpp
@@ -0,0 +1,30 @@
+/*
+ * imagen_de_tablero.cpp
+ *
+ *  Created on: Oct 21, 2018
+ *      Author: german
+ */
+
+#include "imagen_de_tablero.h"
+#include<string>
+
+const int profundidadDeColorStandar = 8;
+
+ImagenDeTablero::ImagenDeTablero(int alto, int ancho, std::string nombre){
+	this->nombreTableroRepresentado = nombre;
+	this->archivoRepresentante.SetSize(alto, ancho);
+	this->archivoRepresentante.SetBitDepth(profundidadDeColorStandar);
+}
+
+void ImagenDeTablero::representarCelulaViva(int fila, int columna, RGB& colorDeCelula){
+
+}
+
+void ImagenDeTablero::representarCelulaMuerta(int fila, int columna){
+
+}
+
+void ImagenDeTablero::guardarImagenTabero(int NumeroDeTurno){
+	std::string NombreArchivo = nombreTableroRepresentado + std::string(NumeroDeTurno);
+
+}
---
+++ src/imagen_de_tablero.h
@@ -0,0 +1,43 @@
+/*
+ * imagen_de_tablero.h
+ *
+ *  Created on: Oct 21, 2018
+ *      Author: german
+ */
+
+#ifndef IMAGEN_DE_TABLERO_H_
+#define IMAGEN_DE_TABLERO_H_
+
+#include <string>
+#include "RGB.h"
+#include "../libreria/EasyBMP.h"
+
+class ImagenDeTablero{
+private:
+	std::string nombreTableroRepresentado;
+	BMP archivoRepresentante;
+public:
+	/*
+	 * pre: -
+	 * post: crea un instancia de imagen de tablero con un archivo.bmp de resolucion alto x ancho y un nombre
+	 de tablero igual que el nombre pasado por parametro (va a ser el mismo nombre que el del tablero que representa)
+	 */
+	ImagenDeTablero(int alto, int ancho, std::string nombre);
+	/*
+	 * pre: los valores de fila y columna que se pasan deben estar dentro de las dimensiones del tablero de origen
+	 * post: se pinta el pixel que representa a la celula en cuestion con el color recibido
+	 */
+	void representarCelulaViva(int fila, int columna, RGB& color);
+	/*
+	 * pre: los valores de fila y columna que se pasan deben estar dentro de las dimensiones del tablero de origen
+	 * post: deja el pixel que representa a la celula en cuestion en blanco (el color que representa a la muerte)
+	 */
+	void representarCelulaMuerta(int fila, int columna);
+	/*
+	 * pre: -
+	 * post: se guarda un archivo.bmp con la representacion de las celulas en el tablero
+	 */
+	void guardarImagenTabero(int NumeroDeTurno);
+};
+
+#endif /* IMAGEN_DE_TABLERO_H_ */
