dd414eafcd4c221e91293a070a9c1eef23712427
german <germandawi@gmail.com>
2018-10-21 12:43:20 -0300

    metodos representarCelulaViva y representarCelulaMuerta creados

--- src/imagen_de_tablero.cpp
+++ src/imagen_de_tablero.cpp
@@ -9,6 +9,7 @@
 #include<string>
 
 const int profundidadDeColorStandar = 8;
+const int valorRGBPorDefecto = 255;
 
 ImagenDeTablero::ImagenDeTablero(int alto, int ancho, std::string nombre){
 	this->nombreTableroRepresentado = nombre;
@@ -17,11 +18,15 @@ ImagenDeTablero::ImagenDeTablero(int alto, int ancho, std::string nombre){
 }
 
 void ImagenDeTablero::representarCelulaViva(int fila, int columna, RGB& colorDeCelula){
-
+	this->archivoRepresentante(fila, columna)->Red = colorDeCelula.getRed();
+	this->archivoRepresentante(fila, columna)->Green = colorDeCelula.getGreen();
+	this->archivoRepresentante(fila, columna)->Blue = colorDeCelula.getBlue();
 }
 
 void ImagenDeTablero::representarCelulaMuerta(int fila, int columna){
-
+	this->archivoRepresentante(fila, columna)->Red = valorRGBPorDefecto;
+	this->archivoRepresentante(fila, columna)->Green = valorRGBPorDefecto;
+	this->archivoRepresentante(fila, columna)->Blue = valorRGBPorDefecto;
 }
 
 void ImagenDeTablero::guardarImagenTabero(int NumeroDeTurno){
