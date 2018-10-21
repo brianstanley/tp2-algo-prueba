e6bc22fc7a2b3225d47159e628c9bf65b413d71d
german <germandawi@gmail.com>
2018-10-21 09:31:06 -0300

    se cambiaron las sugerencias

--- src/Celula.cpp
+++ src/Celula.cpp
@@ -4,35 +4,38 @@
  *  Created on: Oct 18, 2018
  *      Author: german
  */
-#define ENERGIA_MAX 100
+const int ENERGIA_MAX = 100;
+const bool MUERTA = false;
+const bool VIVA = true;
 
 #include"Celula.h"
 
+
 Celula::Celula(){
-	this->estadoCelula = false;
+	this->estadoCelula = MUERTA;
 	this->energiaCelula = 0;
-	this->colorCelula = 0;
+	this->RGBCelula = RGB();
 }
 
-Celula::Celula(float factorNacimiento, Color colorParcela){
-	this->estadoCelula = true;
-	this->energiaCelula = ENERGIA_MAX * factorNacimiento;
-	this->colorCelula = Color(colorParcela);
+Celula::Celula(float factorNacimientoParecla, RGB RGBParcela){
+	this->estadoCelula = VIVA;
+	this->energiaCelula = ENERGIA_MAX * factorNacimientoParecla;
+	this->RGBCelula = RGB(RGBParcela);
 }
 
 void Celula::morir(){
-	this->estadoCelula = false;
+	this->estadoCelula = MUERTA;
 	this->energiaCelula = 0;
 }
 
-void Celula::nacer(float factorNacimiento, const Color& colorA, const Color& colorB, const Color& colorC){
-	this->estadoCelula = true;
-	this->energiaCelula = ENERGIA_MAX * factorNacimiento;
-	this->colorCelula = colorCelula.calcularPromedioColores(colorA, colorB, colorC);
+void Celula::nacer(float factorNacimientoParecla, const RGB& RGBA, const RGB& RGBB, const RGB& RGBC){
+	this->estadoCelula = VIVA;
+	this->energiaCelula = ENERGIA_MAX * factorNacimientoParecla;
+	RGBCelula.calcularPromedioRGBes(RGBA, RGBB, RGBC);
 }
 
-const Color& Celula:: getColor(){
-	return this->colorCelula;
+const RGB& Celula:: getRGB(){
+	return this->RGBCelula;
 }
 
 bool Celula::getEstado(){
--- src/Celula.h
+++ src/Celula.h
@@ -8,29 +8,35 @@
 #ifndef CELULA_H_
 #define CELULA_H_
 
-#include"color.h"
+#include"RGB.h"
 
 class Celula{
 private:
 	bool estadoCelula;
 	int energiaCelula;
-	Color colorCelula; //Para la implementacion de los colores de la celula y parcela
-					   //propongo la clase Color
+	RGB RGBCelula; //Para la implementacion de los RGBes de la celula y parcela
+					   //propongo la clase RGB
 public:
-	//post: se crea una celula muerta con color nulo y energia 0
+	/*pre: -
+	 * post: se crea una celula muerta con RGB nulo y energia 0*/
 	Celula();
-	//post: se crea una celula viva con el color y su enerigia relativa al
-	//factor de nacimiento de la parcela a la que pertenece
-	Celula(float factorNacimiento, Color colorParcela);
-	//post: se setea el estado de la celula a muerto y la energia a 0
+	/*pre: -
+	 * post: se crea una celula viva con el RGB y su enerigia relativa al
+	factor de nacimiento de la parcela a la que pertenece*/
+	Celula(float factorNacimiento, RGB RGBParcela);
+	/*pre: -
+	 * post: se setea el estado de la celula a muerto y la energia a 0*/
 	void morir();
-	//post: se setea el estado de la celula a vivo ,la energia en funcion del factor de
-	//nacimiento de la parcela y el color como fruto del promedio de los colores de las
-	//tres celulas circundantes
-	void nacer(float factorNacimiento, const Color& colorA, const Color& colorB, const Color& colorC);
-	//post: se devuelve el color de la celula
-	const Color& getColor();
-	//post: se devuelve true si la celula esta viva y false en caso contrario
+	/*pre: -
+	 * post: se setea el estado de la celula a vivo ,la energia en funcion del factor de
+	nacimiento de la parcela y el RGB como fruto del promedio de los RGBes de las
+	tres celulas circundantes*/
+	void nacer(float factorNacimiento, const RGB& RGBA, const RGB& RGBB, const RGB& RGBC);
+	/*pre: -
+	 * post: se devuelve el RGB de la celula*/
+	const RGB& getRGB();
+	/*pre: -
+	 * post: se devuelve true si la celula esta viva y false en caso contrario*/
 	bool getEstado();
 };
 
