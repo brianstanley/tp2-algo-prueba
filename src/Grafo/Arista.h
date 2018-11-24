14de762a270c0f0c2bb75adaff8bd7b1e2f548ea
germandawi <germandawi@gmail.com>
2018-11-24 11:56:17 -0300

    creadas las primitivas de las clases Vertice y Arista

--- src/Grafo/Arista.h
+++ src/Grafo/Arista.h
@@ -5,4 +5,31 @@ private:
 	Tablero* VerticeEmisor;
 	Tablero* VerticeReceptor;
 	int pesoArista;
+
+public:
+
+	/*
+	 * pre: -
+	 * post: se crea un objeto de la clase arista con un peso nulo y los vertices emisor y receptor
+	 * pasados por parametro
+	 */
+	Arista(Tablero* VerticeEmisor, Tablero* VerticeReceptor);
+
+	/*
+	 * pre:
+	 * post: se incrementa en uno el peso de la arista
+	 */
+	void incrementarPeso();
+
+	/*
+	 * pre: -
+	 * post: se devuelve el peso de la arista
+	 */
+	int getPeso();
+
+	/*
+	 * pre: -
+	 * post: se libera toda la memoria utilizada para la creacion de un objeto de clase Arista
+	 */
+	~Arista();
 };
--- src/Grafo/Vertice.h
+++ src/Grafo/Vertice.h
@@ -3,6 +3,27 @@
 
 class Vertice{
 private:
-	ListaEnlazada<Arista*> AristasDelGrafo;
+	ListaEnlazada<Arista*>* AristasDelGrafo;
 	Tablero* pesoVertice;
+
+public:
+
+	/*
+	 * pre: -
+	 * post: se crea un objeto de la clase vertice con el tablero pasado por parametro como
+	 * peso del vertice
+	 */
+	Vertice(Tablero* tableroAsociado);
+
+	/*
+	 * pre: -
+	 * post: se devuelve el peso del vertice en cuestion
+	 */
+	Tablero* getPeso();
+
+	/*
+	 * pre: -
+	 * post: se libera toda la memoria utilizada para la creacion de un objeto de clase Vertice
+	 */
+	~Vertice();
 };
